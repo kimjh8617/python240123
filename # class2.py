40360ee78abf056553f61190ac80693ca12e5faa
kimjh8617 <kimjh8617@daum.net>
2024-01-24 11:03:05 +0900

    서식지정연습

---
+++ # class2.py
@@ -0,0 +1,19 @@
+# # class2.py
+
+# import DemoSet
+# dir()
+
+
+# from DemoSet import union
+# union([1,2,3])
+
+
+
+# from DemoSet import *
+# __oo (이름숨김)
+
+# BankAccount 
+
+import DemoModule as d1 
+d1.x = 1000
+import DemoModule as d2
--- test6.py
+++ test6.py
@@ -1,1 +1,10 @@
 print("aaaa")
+
+
+
+
+
+#from 패키지명 import 모듈명
+#from 패키지명 import *
+
+
