aadc4b33f79b8f66f51afae124a71d053fec5490
kimjh8617 <kimjh8617@daum.net>
2024-01-26 10:18:53 +0900

    데모2

--- wab2.py
+++ wab2.py
@@ -5,28 +5,28 @@ import requests as rq
 #크롤링
 from bs4 import BeautifulSoup
 
-#주소
-url = "https://www.daangn.com/fleamarket/"
-response = rq.get(url)
+        #주소
+        url = "https://www.daangn.com/fleamarket/"
+        response = rq.get(url)
 
-#검색이 용이한 객체
-soup = BeautifulSoup(response.text, "html.parser")
+        #검색이 용이한 객체
+        soup = BeautifulSoup(response.text, "html.parser")
 
-#검색
-#파일에 저장
-f = open("daangn.txt", "wt", encoding="utf-8")
-posts = soup.find_all("div", attrs={"class" : "card-desc"})
-for post in posts:
-    titleElem= post.find("h2", attrs={"class" : "card-title"})
-    priceElem= post.find("div", attrs={"class" : "card-price"})
-    addrElem= post.find("div", attrs={"class" : "card-region-name"})
-    title = titleElem.text.strip().replace("\n", "")
-    price = priceElem.text.strip().replace("\n", "")
-    addr = addrElem.text.strip().replace("\n", "")
-    print(f"{title}, {price}, {addr}")
-    f.write(f"{title}, {price}, {addr}\n")
+        #검색
+        #파일에 저장
+        f = open("daangn.txt", "wt", encoding="utf-8")
+        posts = soup.find_all("div", attrs={"class" : "card-desc"})
+        for post in posts:
+            titleElem= post.find("h2", attrs={"class" : "card-title"})
+            priceElem= post.find("div", attrs={"class" : "card-price"})
+            addrElem= post.find("div", attrs={"class" : "card-region-name"})
+            title = titleElem.text.strip().replace("\n", "")
+            price = priceElem.text.strip().replace("\n", "")
+            addr = addrElem.text.strip().replace("\n", "")
+            print(f"{title}, {price}, {addr}")
+            f.write(f"{title}, {price}, {addr}\n")
 
-f.close()
+        f.close()
 
 
 
